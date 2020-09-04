0eebf5e1df1e1f95ebb8409fd1920697f8185333
kaooru <amesodawater@gmail.com>
2020-09-04 18:53:22 +0900

    Bookers finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,48 @@
 class BooksController < ApplicationController
-  def top
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def index
-  end
-
-  def show
+  	@books = Book.all.order("id ASC")
+  	@book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  	  flash[:notice] = "successfully"
+  	  redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice] = "successfully updated"
+  	  redirect_to book_path(@book.id)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	flash[:notice] = "successfully destroyed"
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  root "books#top"
+  resources :books
+  patch "books/:id" => "books#update", as: "update_book"
+  delete "books/:id" => "books#destroy", as: "destroy_book"
+  # get "/books" => "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
